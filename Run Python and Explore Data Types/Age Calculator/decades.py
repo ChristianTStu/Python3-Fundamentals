5e96a0a5e4127b3e342132688090984b312fefa6
ChristianTStu <Christiantstu@gmail.com>
2023-10-08 15:11:02 -0500

    Added Comments

--- Run Python and Explore Data Types/Age Calculator/decades.py
+++ Run Python and Explore Data Types/Age Calculator/decades.py
@@ -1,5 +1,7 @@
 age = int(input("How Old Are You?\n"))
 decades = age // 10
+
+# Modulo can show the reminder of a value, and we store this remainder as a seperate variable "years"
 years = age % 10
 
 print("You are " + str(decades) + 
