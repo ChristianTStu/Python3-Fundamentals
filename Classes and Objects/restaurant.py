e11b2c3a3509d7da403667158a0a5ea7e3c694ce
ChristianTStu <christiantstu@gmail.com>
2024-02-14 11:03:36 -0600

    Add user class

--- Classes and Objects/restaurant.py
+++ Classes and Objects/restaurant.py
@@ -21,3 +21,24 @@ old_resturant.open_resturant()
 your_resturant = Resturant('Bagger Daves', 'Burger')
 your_resturant.describe_resturant()
 your_resturant.open_resturant()
+
+class User:
+
+    def __init__(self, first_name, last_name, age, geolocation):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.geolocation = geolocation
+
+    def describe_user(self):
+        print(f'{self.first_name} {self.last_name} is {self.age} years old and lives in {self.geolocation}.')
+
+    def greet_user(self):
+        print(f'Welcome {self.first_name}! Would you like to play a game?')
+
+christian = User('Christian', 'Stuart', 26, 'North America')
+jon = User('Jon', 'Noel', 28, 'Australia')
+bobby = User('Robert', 'Wilkens', 24, 'England')
+
+christian.describe_user()
+christian.greet_user()
