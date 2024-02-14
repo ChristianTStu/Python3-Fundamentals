2a1012820fef138068f3ed5236433eb88c16291c
ChristianTStu <christiantstu@gmail.com>
2024-02-14 12:44:57 -0600

    Completed resturant.py

--- Classes and Objects/restaurant.py
+++ Classes and Objects/restaurant.py
@@ -1,44 +1,76 @@
-class Resturant:
+class Restaurant:
 
     def __init__(self, name, cuisine_type):
         self.name = name
         self.cuisine_type = cuisine_type
+        self.customer_count = 0
 
-    def describe_resturant(self):
+    def describe_restaurant(self):
         print(f'{self.name} serves {self.cuisine_type} type food.')
 
-    def open_resturant(self):
+    def open_restaurant(self):
         print(f'{self.name} is open for business!')
+    
+    def set_number_served(self, number):
+        self.number_served = number
 
-new_resturant = Resturant('FlameOn Habachi', 'Asian Fusion')
-new_resturant.describe_resturant()
-new_resturant.open_resturant()
+    def increment_number_served(self, increment):
+        self.number_served += increment
 
-old_resturant = Resturant('Total Noodle', 'Pasta')
-old_resturant.describe_resturant()
-old_resturant.open_resturant()
+    def display_number_served(self):
+        print(f'This restaurant has served {self.number_served} customers.')
 
-your_resturant = Resturant('Bagger Daves', 'Burger')
-your_resturant.describe_resturant()
-your_resturant.open_resturant()
 
 class User:
 
-    def __init__(self, first_name, last_name, age, geolocation):
+    def __init__(self, first_name, last_name, username, email):
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
-        self.geolocation = geolocation
+        self.username = username
+        self.email = email
+        self.login_attempts = 0
 
     def describe_user(self):
-        print(f'{self.first_name} {self.last_name} is {self.age} years old and lives in {self.geolocation}.')
+        print(f'User: {self.first_name} {self.last_name}')
+        print(f'Username: {self.username}')
+        print(f'Email: {self.email}')
 
     def greet_user(self):
         print(f'Welcome {self.first_name}! Would you like to play a game?')
 
-christian = User('Christian', 'Stuart', 26, 'North America')
-jon = User('Jon', 'Noel', 28, 'Australia')
-bobby = User('Robert', 'Wilkens', 24, 'England')
+    def increment_login_attempts(self):
+        self.login_attempts += 1
 
-christian.describe_user()
-christian.greet_user()
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+
+# Testing Restaurant class
+restaurant = Restaurant('FlameOn Habachi', 'Asian Fusion')
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+
+# Setting and displaying number served
+restaurant.set_number_served(50)
+restaurant.display_number_served()
+
+# Incrementing number served and displaying again
+restaurant.increment_number_served(20)
+restaurant.display_number_served()
+
+# Testing User class
+user = User('John', 'Doe', 'johndoe', 'john@example.com')
+
+# Incrementing login attempts
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+
+# Displaying login attempts
+print(f'Login attempts: {user.login_attempts}')
+
+# Resetting login attempts
+user.reset_login_attempts()
+
+# Displaying login attempts after reset
+print(f'Login attempts after reset: {user.login_attempts}')
